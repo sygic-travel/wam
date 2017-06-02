e5f9344f34adc4d79ec05a860961d5c28bf9372b
Rostislav Wolny <rostislav.wolny@sygictravel.com>
2017-06-02 10:48:59 +0200

    limited places

--- src/places/index.js
+++ src/places/index.js
@@ -1,10 +1,16 @@
 export function processPlaces(placesData, zoom, heading) {
 
-	console.log(zoom, heading);
+	// console.log(zoom, heading);
 	let bottom = heading - 15;
 	if (bottom < 0) {
 		bottom = 360 - bottom;
 	}
+	const BIG_LIMIT = 6;
+	const MEDIUM_LIMIT = 8;
+	const SMALL_LIMIT = 10;
+	let bigCount = 0;
+	let mediumCount = 0;
+	let smallCount = 0;
 	const top = heading + 15;
 	return placesData.map((placeData) => {
 		if (placeData.angle >= bottom && placeData.angle <= top) {
@@ -37,6 +43,27 @@ export function processPlaces(placesData, zoom, heading) {
 			}
 			if (zoom > 1) {
 				placeData.markerSize = 'big';
+			}
+		}
+
+		if (placeData.markerSize === 'big' && placeData.displayMargin !== null) {
+			bigCount++;
+			if (bigCount > BIG_LIMIT) {
+				placeData.displayMargin = null;
+			}
+		}
+
+		if (placeData.markerSize === 'medium' && placeData.displayMargin !== null) {
+			mediumCount++;
+			if (mediumCount > MEDIUM_LIMIT) {
+				placeData.displayMargin = null;
+			}
+		}
+
+		if (placeData.markerSize === 'small' && placeData.displayMargin !== null) {
+			mediumCount++;
+			if (mediumCount > SMALL_LIMIT) {
+				placeData.displayMargin = null;
 			}
 		}
 
